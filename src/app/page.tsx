6a2df60ddbe447f7c415563c69eb3b2ef7880167
Farouk Charkas <farouk.charkas@gmail.com>
2024-04-23 03:07:09 -0400

    Adjust landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,7 @@ import Logo from "../../public/GlobeWit.svg";
 export default function Start() {
   return (
     <main className="flex min-h-screen flex-col items-center noise justify-center bg-black">
-      <div className="flex w-full h-full">
-        <div className="flex-1">&nbsp;</div>
+      <div className="flex flex-col justify-center items-center w-full h-full">
         <div className="flex flex-1 flex-col items-center">
           <Image
             src="/GlobeWit.svg"
@@ -16,9 +15,9 @@ export default function Start() {
             height={200}
           />
         </div>
-        <div className="flex-1"></div>
+        <p className="mb-8">Think you know the globe? No you don&apos;t.</p>
         <Link href={"/game"}>
-          <Button>Play</Button>
+          <Button className="w-[150px]">Play</Button>
         </Link>
       </div>
     </main>
